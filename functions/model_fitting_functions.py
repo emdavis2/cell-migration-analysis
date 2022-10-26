4b4d54cc1fa136a8b0f8aa2e3b5717640bf823c7
emdavis2 <elizabethmdav@gmail.com>
2022-10-26 14:47:59 -0400

    updated functions to use defined error function in that file

--- functions/model_fitting_functions.py
+++ functions/model_fitting_functions.py
@@ -30,14 +30,14 @@ def run_sim_get_err(poslagaverage_data, run_sim_fn, min_track_length):
     return acf_vel_err
 
 
-def perform_gridsearch_2params(err_fn, std_dev_w_vals, std_dev_theta_vals):
+def perform_gridsearch_2params(poslagaverage_data, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
     min_err = 100
     index_w = 0
     index_theta = 0
 
     for ind_w, std_dev_w in enumerate(std_dev_w_vals):
         for ind_t,std_dev_theta in enumerate(std_dev_theta_vals):
-            err = err_fn
+            err = run_sim_get_err(poslagaverage_data, run_PRWpolaritybias_sim(Nwalkers, dt, time, std_dev_w, std_dev_theta), min_track_length)
             if err < min_err:
                 min_err = err
                 index_w = ind_w
@@ -48,13 +48,12 @@ def perform_gridsearch_2params(err_fn, std_dev_w_vals, std_dev_theta_vals):
     return min_err, std_dev_w_vals[index_w], std_dev_theta_vals[index_theta]
 
 
-def perform_gridsearch_1param1(err_fn, std_dev_theta_vals):
+def perform_gridsearch_1param(poslagaverage_data, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
     min_err = 100
-    index_w = 0
     index_theta = 0
 
     for ind_t, std_dev_theta in enumerate(std_dev_theta_vals):
-        err = err_fn
+        err = run_sim_get_err(poslagaverage_data, run_PRW_sim(Nwalkers, dt, time, std_dev_theta), min_track_length)
         if err < min_err:
             min_err = err
             index_theta = ind_t
