cbd30e579c6cfca2689e2a8bb30cd964a237fdde
emdavis2 <elizabethmdav@gmail.com>
2022-10-26 14:02:01 -0400

    created functions to run grid search to fit model

---
+++ functions/model_fitting_functions.py
@@ -0,0 +1,64 @@
+from functions.PRW_model_functions import *
+from functions.acf_functions import *
+from functions.compile_data_tracks_function import *
+
+import numpy as np 
+
+def run_sim_get_err(poslagaverage_data, run_sim_fn, track_length):
+    #np.random.seed(20)
+
+    data_sim = run_sim_fn
+
+    poslagaverage = np.zeros(300)
+    all_ac = []
+    for df in data_sim:
+        track=df
+        combined = make_comb_df(track['vx'].to_list()[2:track_length-2],track['vy'].to_list()[2:track_length-2])
+        combined = combined.dropna()
+        poslagsmean, Nposlags, neglagsmean, Nneglags = xcorr_vector(combined)
+
+        #remove nans here
+        poslagsmean[np.isnan(poslagsmean)] = 0
+        all_ac.append(poslagsmean)
+        poslagaverage[0:len(poslagsmean)] += poslagsmean #Nposlags*poslagsmean
+    poslagaverage /= len(data_sim) #Nposlagtotal 
+
+    poslagaverage_sim = poslagaverage[0:track_length-4]
+
+    acf_vel_err = np.sum(np.abs(poslagaverage_data - poslagaverage_sim))
+
+    return acf_vel_err
+
+
+def perform_gridsearch_2params(err_fn, std_dev_w_vals, std_dev_theta_vals):
+    min_err = 100
+    index_w = 0
+    index_theta = 0
+
+    for ind_w, std_dev_w in enumerate(std_dev_w_vals):
+        for ind_t,std_dev_theta in enumerate(std_dev_theta_vals):
+            err = err_fn
+            if err < min_err:
+                min_err = err
+                index_w = ind_w
+                index_theta = ind_t
+            else:
+                continue
+    
+    return min_err, std_dev_w_vals[index_w], std_dev_theta_vals[index_theta]
+
+
+def perform_gridsearch_1param1(err_fn, std_dev_theta_vals):
+    min_err = 100
+    index_w = 0
+    index_theta = 0
+
+    for ind_t, std_dev_theta in enumerate(std_dev_theta_vals):
+        err = err_fn
+        if err < min_err:
+            min_err = err
+            index_theta = ind_t
+        else:
+            continue
+    
+    return min_err, std_dev_theta_vals[index_theta]
