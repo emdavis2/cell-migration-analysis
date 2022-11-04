737f8a6f0e3860ac8c9716ee1e558ead67c5620b
emdavis2 <elizabethmdav@gmail.com>
2022-11-04 11:46:33 -0400

    add msd function for one cell

--- functions/msd_functions.py
+++ functions/msd_functions.py
@@ -28,3 +28,14 @@ def calc_MSD_sim(data,track_length):
       msd_onecell.append(np.average(poslags))
     msd_allcells.append(msd_onecell[0:track_length])
   return np.average(msd_allcells,axis=0)
+
+def calc_MSD_onecell(data,track_length):
+  length = len(data)
+  msd_onecell = []
+  for lag in range(length):
+    x = np.array(data['x'].to_list())
+    y = np.array(data['y'].to_list())
+    r = np.sqrt(x**2 + y**2)
+    poslags =  (r[lag:] - r[0:length-lag])**2
+    msd_onecell.append(np.average(poslags))
+  return np.array(msd_onecell[:track_length])
