a4b0b92e69697c80fcbe490dc069a322e8961296
emdavis2 <elizabethmdav@gmail.com>
2022-11-06 19:52:20 -0500

    made an extra input in perform_gridsearch functions so way fit is performed can be modified - fit to vel acf or msd etc.

--- functions/model_fitting_functions.py
+++ functions/model_fitting_functions.py
@@ -11,7 +11,7 @@ def make_comb_df(vx, vy):
     combined = pd.concat([vel_df[['vx','vy']].reset_index(drop=True), vel_df[['vx','vy']].reset_index(drop=True)], axis = 1 )
     return combined
 
-def run_sim_get_err(poslagaverage_data, run_sim_fn, min_track_length):
+def run_sim_get_velacf_err(poslagaverage_data, run_sim_fn, min_track_length):
     #np.random.seed(20)
 
     data_sim = run_sim_fn
@@ -37,14 +37,14 @@ def run_sim_get_err(poslagaverage_data, run_sim_fn, min_track_length):
     return acf_vel_err
 
 
-def perform_gridsearch_2params(poslagaverage_data, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+def perform_gridsearch_2params(poslagaverage_data, run_sim_err_fn, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
     min_err = 100
     index_w = 0
     index_theta = 0
 
     for ind_w, std_dev_w in enumerate(std_dev_w_vals):
         for ind_t,std_dev_theta in enumerate(std_dev_theta_vals):
-            err = run_sim_get_err(poslagaverage_data, run_PRWpolaritybias_sim(Nwalkers, dt, time, std_dev_w, std_dev_theta), min_track_length)
+            err = run_sim_err_fn(poslagaverage_data, run_PRWpolaritybias_sim(Nwalkers, dt, time, std_dev_w, std_dev_theta), min_track_length)
             if err < min_err:
                 min_err = err
                 index_w = ind_w
@@ -55,12 +55,12 @@ def perform_gridsearch_2params(poslagaverage_data, std_dev_w_vals, std_dev_theta
     return min_err, std_dev_w_vals[index_w], std_dev_theta_vals[index_theta]
 
 
-def perform_gridsearch_1param(poslagaverage_data, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+def perform_gridsearch_1param(poslagaverage_data, run_sim_err_fn, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
     min_err = 100
     index_theta = 0
 
     for ind_t, std_dev_theta in enumerate(std_dev_theta_vals):
-        err = run_sim_get_err(poslagaverage_data, run_PRW_sim(Nwalkers, dt, time, std_dev_theta), min_track_length)
+        err = run_sim_err_fn(poslagaverage_data, run_PRW_sim(Nwalkers, dt, time, std_dev_theta), min_track_length)
         if err < min_err:
             min_err = err
             index_theta = ind_t
