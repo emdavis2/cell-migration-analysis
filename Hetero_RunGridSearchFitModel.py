2734b72c43275c7ff9a53c107ac200f63ec39bf4
emdavis2 <elizabethmdav@gmail.com>
2022-11-07 14:15:58 -0500

    added fitting for MSD

--- Hetero_RunGridSearchFitModel.py
+++ Hetero_RunGridSearchFitModel.py
@@ -20,34 +20,23 @@ Nwalkers = int(sys.argv[6]) #113
 
 model_type = str(sys.argv[7]) #PRW or PRW_PB
 
-err_fn = str(sys.argv[8]) #run_sim_get_velacf_err or run_sim_get_msd_err
+err_fn = str(sys.argv[8]) #vel_acf or MSD
 
 tracks_region, tracks_geo_region, region_cells, region_endpointcells = compile_data_tracks(treatment, min_track_length, region)
-
-#autocorrelation velocity for data
-poslagaverage_data = []
-for df in tracks_geo_region:
-  track=df
-  combined = pd.concat([track[['vx','vy']].reset_index(drop=True), track[['vx','vy']].reset_index(drop=True)], axis = 1 )
-  poslagsmean, Nposlags, neglagsmean, Nneglags = xcorr_vector(combined, min_track_length)
-
-  #remove nans here
-  poslagsmean[np.isnan(poslagsmean)] = 0
-  poslagaverage_data.append(poslagsmean)
 
 #perform grid search
 if model_type == 'PRW':
     std_dev_theta_vals = np.linspace(0.1, 2, 10)
-    tot_err, std_dev_theta = perform_gridsearch_1param(poslagaverage_data, err_fn, std_dev_theta_vals, Nwalkers, dt, time, min_track_length)
-    with open(r'hetero_model/model_params_{}_{}.txt'.format(region, model_type), 'w') as f:
+    tot_err, std_dev_theta = perform_gridsearch_1param(tracks_region, tracks_geo_region, err_fn, std_dev_theta_vals, Nwalkers, dt, time, min_track_length)
+    with open(r'hetero_model/model_params_{}_{}_{}.txt'.format(region, model_type, err_fn), 'w') as f:
         f.write('min_err={}'.format(str(tot_err)))
         f.write('\n')
         f.write('std_dev_theta={}'.format(str(std_dev_theta)))
 elif model_type == 'PRW_PB':
     std_dev_w_vals = np.linspace(0.1, 2, 10)
     std_dev_theta_vals = np.linspace(0.1, 2, 10)
-    tot_err, std_dev_w, std_dev_theta = perform_gridsearch_2params(poslagaverage_data, err_fn, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length)
-    with open(r'hetero_model/model_params_{}_{}.txt'.format(region, model_type), 'w') as f:
+    tot_err, std_dev_w, std_dev_theta = perform_gridsearch_2params(tracks_region, tracks_geo_region, err_fn, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length)
+    with open(r'hetero_model/model_params_{}_{}_{}.txt'.format(region, model_type, err_fn), 'w') as f:
         f.write('min_err={}'.format(str(tot_err)))
         f.write('\n')
         f.write('std_dev_w={}'.format(str(std_dev_w)))
--- functions/hetero_model_fitting_functions.py
+++ functions/hetero_model_fitting_functions.py
@@ -1,6 +1,7 @@
 from functions.PRW_model_functions import *
 from functions.PRWpolaritybias_model_functions import *
 from functions.acf_functions import *
+from functions.msd_functions import *
 
 import numpy as np 
 import pandas as pd
@@ -11,7 +12,13 @@ def make_comb_df(vx, vy):
     combined = pd.concat([vel_df[['vx','vy']].reset_index(drop=True), vel_df[['vx','vy']].reset_index(drop=True)], axis = 1 )
     return combined
 
-def run_sim_get_velacf_err(poslagsmean_data, run_sim_fn, min_track_length):
+def run_sim_get_velacf_err(tracks_region, tracks_geo_region, run_sim_fn, min_track_length, walker):
+    #autocorrelation velocity for data
+    track_data = tracks_geo_region[walker]
+    combined_data = pd.concat([track_data[['vx','vy']].reset_index(drop=True), track_data[['vx','vy']].reset_index(drop=True)], axis = 1 )
+    poslagsmean_data, Nposlags, neglagsmean, Nneglags = xcorr_vector(combined_data, min_track_length)
+    poslagsmean_data[np.isnan(poslagsmean_data)] = 0
+
     track = run_sim_fn[0]
 
     combined = make_comb_df(track['vx'].to_list()[2:min_track_length-2],track['vy'].to_list()[2:min_track_length-2])
@@ -25,8 +32,23 @@ def run_sim_get_velacf_err(poslagsmean_data, run_sim_fn, min_track_length):
 
     return acf_vel_err
 
+def run_sim_get_MSD_err(tracks_region, tracks_geo_region, run_sim_fn, min_track_length, walker):
 
-def perform_gridsearch_2params(poslagaverage_data, run_sim_err_fn, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+  data_pos = {'x': tracks_region[walker][:,0],'y':tracks_region[walker][:,1]}
+  datapos_df = pd.DataFrame(data=data_pos)
+  MSD_data_onecell = calc_MSD_onecell(datapos_df, min_track_length)
+
+  data_sim = run_sim_fn[0]
+
+  MSD_sim = calc_MSD_onecell(data_sim, min_track_length)
+
+  MSD_err = np.sum(np.abs(MSD_data_onecell - MSD_sim))
+
+  return MSD_err
+
+
+def perform_gridsearch_2params(tracks_region, tracks_geo_region, run_sim_err_fn, std_dev_w_vals, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+    dispatcher = {'vel_acf': run_sim_get_velacf_err, 'MSD': run_sim_get_MSD_err}
     tot_err = 0
     index_w_allcells = []
     index_theta_allcells = []
@@ -37,7 +59,7 @@ def perform_gridsearch_2params(poslagaverage_data, run_sim_err_fn, std_dev_w_val
 
         for ind_w, std_dev_w in enumerate(std_dev_w_vals):
             for ind_t,std_dev_theta in enumerate(std_dev_theta_vals):
-                err = run_sim_err_fn(poslagaverage_data[walker], run_PRWpolaritybias_sim(1, dt, time, std_dev_w, std_dev_theta), min_track_length)
+                err = dispatcher[run_sim_err_fn](tracks_region, tracks_geo_region, run_PRWpolaritybias_sim(1, dt, time, std_dev_w, std_dev_theta), min_track_length, walker)
                 if err < min_err:
                     min_err = err
                     index_w = ind_w
@@ -54,7 +76,8 @@ def perform_gridsearch_2params(poslagaverage_data, run_sim_err_fn, std_dev_w_val
     return tot_err, w_list, theta_list
 
 
-def perform_gridsearch_1param(poslagaverage_data, run_sim_err_fn, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+def perform_gridsearch_1param(tracks_region, tracks_geo_region, run_sim_err_fn, std_dev_theta_vals, Nwalkers, dt, time, min_track_length):
+    dispatcher = {'vel_acf': run_sim_get_velacf_err, 'MSD': run_sim_get_MSD_err}
     tot_err = 0
     index_theta_allcells = []
     for walker in range(Nwalkers):
@@ -62,7 +85,7 @@ def perform_gridsearch_1param(poslagaverage_data, run_sim_err_fn, std_dev_theta_
         index_theta = 0
 
         for ind_t, std_dev_theta in enumerate(std_dev_theta_vals):
-            err = run_sim_err_fn(poslagaverage_data[walker], run_PRW_sim(1, dt, time, std_dev_theta), min_track_length)
+            err = dispatcher[run_sim_err_fn](tracks_region, tracks_geo_region, run_PRW_sim(1, dt, time, std_dev_theta), min_track_length, walker)
             if err < min_err:
                 min_err = err
                 index_theta = ind_t
