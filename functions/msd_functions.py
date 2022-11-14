edf07801db262b9d34405248b64c5966480ba94d
emdavis2 <elizabethmdav@gmail.com>
2022-11-14 14:47:56 -0500

    changed the way MSD is calculated

--- functions/msd_functions.py
+++ functions/msd_functions.py
@@ -8,8 +8,8 @@ def calc_MSD(data,track_length):
     for lag in range(length):
       x = np.array(data[cell][:,0])
       y = np.array(data[cell][:,1])
-      r = np.sqrt(x**2 + y**2)
-      poslags =  (r[lag:] - r[0:length-lag])**2
+      #r = np.sqrt(x**2 + y**2)
+      poslags =  (x[lag:] - x[0:length-lag])**2 + (y[lag:] - y[0:length-lag])**2
       msd_onecell.append(np.average(poslags))
     msd_allcells.append(msd_onecell[0:track_length])
   return np.average(msd_allcells,axis=0)
@@ -23,8 +23,8 @@ def calc_MSD_sim(data,track_length):
     for lag in range(length):
       x = np.array(data[cell]['x'].to_list())
       y = np.array(data[cell]['y'].to_list())
-      r = np.sqrt(x**2 + y**2)
-      poslags =  (r[lag:] - r[0:length-lag])**2
+      #r = np.sqrt(x**2 + y**2)
+      poslags =  (x[lag:] - x[0:length-lag])**2 + (y[lag:] - y[0:length-lag])**2
       msd_onecell.append(np.average(poslags))
     msd_allcells.append(msd_onecell[0:track_length])
   return np.average(msd_allcells,axis=0)
@@ -35,7 +35,7 @@ def calc_MSD_onecell(data,track_length):
   for lag in range(length):
     x = np.array(data['x'].to_list())
     y = np.array(data['y'].to_list())
-    r = np.sqrt(x**2 + y**2)
-    poslags =  (r[lag:] - r[0:length-lag])**2
+    #r = np.sqrt(x**2 + y**2)
+    poslags =  (x[lag:] - x[0:length-lag])**2 + (y[lag:] - y[0:length-lag])**2
     msd_onecell.append(np.average(poslags))
   return np.array(msd_onecell[:track_length])
