d65e4f67d8d94293cb0fab0722a6029236b810a6
emdavis2 <elizabethmdav@gmail.com>
2023-08-30 22:14:00 -0400

    added plots for D/T and speed with window size 5

--- functions/plot_functions.py
+++ functions/plot_functions.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.stats import f_oneway
+from scipy.stats import ttest_ind
+from scipy.stats import kstest
 
 #Get velocity for specified region using corresponding tracks_geo_region list of dataframes
 #returns velocity (and x and y components) in terms of um/min as single list for entire region
@@ -42,6 +44,20 @@ def ExtractAbsskewSolidityEccenArea(tracks_geo_region, pixel_size):
     return absskew_region, solidity_region, eccentricity_region, area_region
 
 
+#Get D/T of window size 5 for specified region using corresponding tracks_geo_region list of dataframes
+#returns absskew and solidity as single lists for entire region
+def ExtractDoverTwin5_speedwin5(tracks_geo_region, sampling_t):
+    DoverTwin5_region = []
+    speedwin5_region = []
+    for i in range(len(tracks_geo_region)):
+        DoverTwin5_region.append(tracks_geo_region[i]['DoTwin5'].dropna().tolist())
+        speedwin5_region.append(tracks_geo_region[i]['pathlen_win5'].dropna().tolist())
+
+    DoverTwin5_region = np.concatenate(DoverTwin5_region).ravel()
+    speedwin5_region = ((np.concatenate(speedwin5_region).ravel())/5)/sampling_t
+
+    return DoverTwin5_region, speedwin5_region
+
 #Plot a histogram of data of interest for one region
 def HistogramPlot(data_to_plot, region, metric, bin_num, save_path, sentinel_name):
     plt.hist(data_to_plot,bins=bin_num)
@@ -59,7 +75,9 @@ def BoxplotPlot(region_list, metric_list, metric, save_path, sentinel_path, pval
     data_bp = {}
     for index,region in enumerate(region_list):
         data_bp[region] = metric_list[region]
-        tstat, pval =  f_oneway(metric_list[region_list[index]],metric_list[region_list[index-1]])
+        #tstat, pval =  f_oneway(metric_list[region_list[index]],metric_list[region_list[index-1]])
+        tstat, pval =  ttest_ind(metric_list[region_list[index]],metric_list[region_list[index-1]])
+        #tstat, pval =  kstest(metric_list[region_list[index]],metric_list[region_list[index-1]])
         pval_file_name.append('{} and {}: {} \n'.format(region_list[index], region_list[index-1], pval))
     data_boxplot = pd.DataFrame({ key:pd.Series(value) for key, value in data_bp.items() })
     sns.boxplot(data=data_boxplot)
@@ -75,6 +93,7 @@ def PlotTrackLengthHist(tracks_geo_region, save_path, sentinel_name):
         lengths = [len(tracks_geo_region[region][i]) for i in range(len(tracks_geo_region[region])) ]
         num_tracks = len(tracks_geo_region[region])
         plt.hist(lengths)
+        plt.xlim(20,250)
         plt.xlabel('track length (number of frames)')
         plt.ylabel('counts')
         plt.title('Track lengths for {} data ({} tracks)'.format(region, num_tracks))
